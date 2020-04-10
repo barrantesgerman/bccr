68f25bbcd497ff32e0bfa30a8a5bd861ccbff3a5
Herman Barrantes <barrantesgerman@gmail.com>
2020-04-10 13:15:16 -0600

    Se agregan anotaciones de Json-B

--- src/main/java/org/habv/bccr/Indicador.java
+++ src/main/java/org/habv/bccr/Indicador.java
@@ -1,6 +1,9 @@
 package org.habv.bccr;
 
 import java.time.OffsetDateTime;
+import javax.json.bind.annotation.JsonbDateFormat;
+import javax.json.bind.annotation.JsonbNumberFormat;
+import javax.json.bind.annotation.JsonbTransient;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -15,11 +18,14 @@ import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Indicador {
 
+    @JsonbTransient
     @XmlElement(name = "COD_INDICADORINTERNO")
     private Integer codigo;
+    @JsonbDateFormat(value = "yyyy-MM-dd'T'HH:mm:ssXXX")
     @XmlElement(name = "DES_FECHA")
     @XmlJavaTypeAdapter(OffsetDateTimeXmlAdapter.class)
     private OffsetDateTime fecha;
+    @JsonbNumberFormat(value = "#,##0.00")
     @XmlElement(name = "NUM_VALOR")
     private Double valor;
 
