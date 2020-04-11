2013964545057f54c7a3a8d1020cd7b43a9efdb1
Herman Barrantes <barrantesgerman@gmail.com>
2020-04-11 14:18:33 -0600

    Correción de imports

--- src/main/java/org/habv/bccr/BccrApplication.java
+++ src/main/java/org/habv/bccr/BccrApplication.java
@@ -6,7 +6,6 @@ import javax.ws.rs.core.Application;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.info.Contact;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
-import org.eclipse.microprofile.openapi.annotations.servers.Server;
 
 /**
  * @author Herman Barrantes
