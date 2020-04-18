12c0a4fab84edbdaebaecab705cf8544965c6cdf
Herman Barrantes <barrantesgerman@gmail.com>
2020-04-18 10:23:48 -0600

    Se agrega consulta histórica por rango de fechas

--- src/main/java/org/habv/bccr/DolarController.java
+++ src/main/java/org/habv/bccr/DolarController.java
@@ -1,17 +1,20 @@
 package org.habv.bccr;
 
+import java.time.LocalDate;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
@@ -74,6 +77,64 @@ public class DolarController {
     public Response dolarVenta() {
         List<Indicador> indicadores = indicadoresService.consultarIndicador(Constantes.DOLAR_VENTA, null, null, SubNivel.N);
         return Response.ok(indicadores.get(0)).build();
+    }
+
+    @Operation(description = "Obtiene el tipo de cambio del dolar para la compra para un rango de fechas")
+    @APIResponse(
+            responseCode = "200",
+            description = "Exito al obtener el tipo de cambio del dolar para el rango de fechas indicado",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(
+                            type = SchemaType.ARRAY,
+                            implementation = Indicador.class)))
+    @APIResponse(
+            responseCode = "400",
+            description = "Error al obtener el tipo de cambio del dolar para el rango de fechas",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(
+                            type = SchemaType.OBJECT,
+                            implementation = Payload.class)))
+    @GET
+    @Path("compra/historico")
+    public Response dolarCompraHistorico(
+            @Parameter(description = "Fecha inicial de la consulta", schema = @Schema(format = "date", type = SchemaType.STRING))
+            @QueryParam("fechaInicial") LocalDate fechaInicial,
+            @Parameter(description = "Fecha final de la consulta", schema = @Schema(format = "date", type = SchemaType.STRING))
+            @QueryParam("fechaFinal") LocalDate fechaFinal
+    ) {
+        List<Indicador> indicadores = indicadoresService.consultarIndicador(Constantes.DOLAR_COMPRA, fechaInicial, fechaFinal, SubNivel.N);
+        return Response.ok(indicadores).build();
+    }
+
+    @Operation(description = "Obtiene el tipo de cambio del dolar para la venta para un rango de fechas")
+    @APIResponse(
+            responseCode = "200",
+            description = "Exito al obtener el tipo de cambio del dolar para el rango de fechas indicado",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(
+                            type = SchemaType.ARRAY,
+                            implementation = Indicador.class)))
+    @APIResponse(
+            responseCode = "400",
+            description = "Error al obtener el tipo de cambio del dolar para el rango de fechas",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(
+                            type = SchemaType.OBJECT,
+                            implementation = Payload.class)))
+    @GET
+    @Path("venta/historico")
+    public Response dolarVentaHistorico(
+            @Parameter(description = "Fecha inicial de la consulta", schema = @Schema(format = "date", type = SchemaType.STRING))
+            @QueryParam("fechaInicial") LocalDate fechaInicial,
+            @Parameter(description = "Fecha final de la consulta", schema = @Schema(format = "date", type = SchemaType.STRING))
+            @QueryParam("fechaFinal") LocalDate fechaFinal
+    ) {
+        List<Indicador> indicadores = indicadoresService.consultarIndicador(Constantes.DOLAR_VENTA, fechaInicial, fechaFinal, SubNivel.N);
+        return Response.ok(indicadores).build();
     }
 
 }
